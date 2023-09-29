b7cc4d4b883650581f7e4d810aba1db5e8363928
Prem <premkhodke.99@gmail.com>
2023-09-29 11:25:13 +0530

    label added

--- index.js
+++ index.js
@@ -3,48 +3,52 @@ const time = new Date().toLocaleDateString("en-us", {
     year: "numeric",
     month: "short",
     day: "numeric"
-  });
-  document.getElementById("time").innerText = time;
-  
-  const list = document.getElementById("todo-list");
-  const form = document.getElementById("task-form");
-  
-  let count = 1;
-  
-  form.addEventListener("submit", (event) => {
+});
+document.getElementById("time").innerText = time;
+
+const list = document.getElementById("todo-list");
+const form = document.getElementById("task-form");
+
+let count = 1;
+
+form.addEventListener("submit", (event) => {
     event.preventDefault();
     const form = event.target;
     const task = form.elements["todo-task"].value;
-  
+
     if (task !== "") {
-      const li = document.createElement("li");
-      li.className = "list-group-item p-3";
-  
-      const check = document.createElement("input");
-      check.type = "checkbox";
-      check.className = "form-check-input me-1";
-      check.id = `todo-${count}`;
-  
-      check.addEventListener("change", function () {
-        if (check.checked) {
-          li.classList.add("text-muted");
-          li.style.textDecoration = "line-through";
-        } else {
-          li.classList.remove("text-muted");
-          li.style.textDecoration = "none";
-        }
-      });
-  
-      count++;
-  
-      li.appendChild(check);
-      li.appendChild(document.createTextNode(" " + task));
-      list.appendChild(li);
+        const li = document.createElement("li");
+        li.className = "list-group-item p-3";
+
+        const check = document.createElement("input");
+        check.type = "checkbox";
+        check.className = "form-check-input me-1";
+        check.id = `todo-${count}`;
+
+        const label = document.createElement("label");
+        label.className = "form-check-label";
+        label.htmlFor = `todo-${count}`;
+        label.innerText = task;
+        
+        check.addEventListener("change", function () {
+            if (check.checked) {
+                label.classList.add("text-muted");
+                label.style.textDecoration = "line-through";
+            } else {
+                label.classList.remove("text-muted");
+                label.style.textDecoration = "none";
+            }
+        });
+        
+        count++;
+
+        li.append(check, label);
+        list.appendChild(li);
     }
-  });
-  
-  let clear = document.getElementById("clear-btn");
-  clear.addEventListener("click", function () {
+});
+
+let clear = document.getElementById("clear-btn");
+clear.addEventListener("click", function () {
     list.innerText = "";
-  });
-  
+});
+
